92df968274a932abc06a91314687213e49d9ab2b
aqestd <amanbeko.a2005@gmail.com>
2022-02-06 20:45:54 +0600

    2

---
+++ app/src/main/java/com/example/myapplication1/Fragment.kt
@@ -0,0 +1,26 @@
+package com.example.myapplication1
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.Navigation
+
+class Fragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+       val view = inflater.inflate(R.layout.fragment_, container, false)
+
+
+        view.setOnClickListener { Navigation.findNavController(view).navigate(R.id.action_blankFragment_to_fragment) }
+        return view
+
+    }
+
+
+
+}
